fb1d4f609cc6ebed51da71dce1db8cdc2c8b7af2
merlox <merunasgrincalaitis@gmail.com>
2019-03-03 11:51:34 +0100

    Simplified escrow by removing state variables

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -13,8 +13,6 @@ interface IERC20 {
 
 contract Escrow {
     address payable public owner;
-    address[] public tokens;
-    mapping(address => uint256) public tokenFunds;
 
     modifier onlyOwner {
         require(msg.sender == owner, 'You must be the owner to execute that function');
@@ -25,16 +23,10 @@ contract Escrow {
     function () external payable {}
 
     /// @notice To setup the initial tokens that the user will store when creating the escrow
-    /// @param _initialTokens The initial tokens that he'll use for the DAX
-    /// @param _initialFunds The funds for each token in order so that the contract knows how many tokens he sent here
-    constructor (address[] memory _initialTokens, uint256[] memory _initialFunds, address payable _owner) public {
-        require(_initialTokens.length <= 100, 'You cant send more than 100 initial tokens');
+    /// @param _owner The address that will be the owner of this escrow, must be the owner of the tokens
+    constructor (address payable _owner) public {
         require(_owner != address(0), 'The owner address must be set');
         owner = _owner;
-        tokens = _initialTokens;
-        for(uint256 i = 0; i < _initialTokens.length; i++) {
-            tokenFunds[_initialTokens[i]] = _initialFunds[i];
-        }
     }
 
     /// @notice To receive the funds from this contract to the owner of it
