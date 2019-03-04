fb8c91b0294fd9350cfa559649032ec25f05c676
merlox <merunasgrincalaitis@gmail.com>
2019-03-04 16:36:52 +0100

    Creating the sidebar

--- src/index.js
+++ src/index.js
@@ -10,41 +10,44 @@ class Main extends React.Component {
     render() {
         return (
             <div>
+                <Sidebar />
             </div>
         )
     }
 }
+
 /// Create the basic sidebar html, then we'll add the style css
 // The sidebar where you take all your actions
 class Sidebar extends React.Component {
     constructor() {
         super()
+        this.state = {
+            showLimitOrderInput: false
+        }
     }
 
     render() {
         return (
             <div className="sidebar">
-                <div className="selected-asset">ETH - BAT</div>
-                <div>
-                    <p>You own</p>
-                    ETH: 10
-                    BAT: 200
-                </div>
-                <div>
-                    <button>Deposit</button>
-                    <button>Withdraw</button>
-                </div>
-                <div>
-                    <button>Buy</button>
-                    <button>Sell</button>
-                    <select>
-                        <option value="market-order">Market Order</option>
-                        <option value="limit-order">Limit Order</option>
-                    </select>
-                    <div>
-                        <input ref="limit-order-amount" type="number" placeholder="amount"/>
-                    </div>
-                </div>
+                <div className="selected-assets-title">Selected assets:</div>
+                <div className="selected-asset-one">ETH</div>
+                <div className="selected-asset-two">BAT</div>
+                <div className="your-portfolio">Your portfolio:</div>
+                <div className="grid-center">ETH:</div><div className="grid-center">10</div>
+                <div className="grid-center">BAT:</div><div className="grid-center">200</div>
+                <div className="actions">Actions:</div>
+                <button>Deposit</button>
+                <button>Withdraw</button>
+                <button>Buy</button>
+                <button>Sell</button>
+                <select defaultValue="market-order" onChange={selected => {
+                    if(selected.target.value == 'limit-order') this.setState({showLimitOrderInput: true})
+                    else this.setState({showLimitOrderInput: false})
+                }}>
+                    <option value="market-order">Market Order</option>
+                    <option value="limit-order">Limit Order</option>
+                </select>
+                <input ref="limit-order-amount" className={this.state.showLimitOrderInput ? '' : 'hidden'} type="number" placeholder="Price to buy or sell at..."/>
             </div>
         )
     }
