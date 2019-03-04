c9b5c18a7234303f432160cbdd9afacb75e16e68
merlox <merunasgrincalaitis@gmail.com>
2019-03-04 10:47:47 +0100

    Working on the design

--- src/index.js
+++ src/index.js
@@ -9,7 +9,71 @@ class Main extends React.Component {
 
     render() {
         return (
-            <div>The project has been setup. Remember to npm install and webpack -w -d to see your changes.</div>
+            <div>
+            </div>
+        )
+    }
+}
+/// Create the basic sidebar html, then we'll add the style css
+// The sidebar where you take all your actions
+class Sidebar extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div className="sidebar">
+                <div className="selected-asset">ETH - BAT</div>
+                <div>
+                    <p>You own</p>
+                    ETH: 10
+                    BAT: 200
+                </div>
+                <div>
+                    <button>Deposit</button>
+                    <button>Withdraw</button>
+                </div>
+                <div>
+                    <button>Buy</button>
+                    <button>Sell</button>
+                    <select>
+                        <option value="market-order">Market Order</option>
+                        <option value="limit-order">Limit Order</option>
+                    </select>
+                    <div>
+                        <input ref="limit-order-amount" type="number" placeholder="amount"/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+// The main section to see live trades taking place
+class Trades extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+// Past historical trades
+class History extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
         )
     }
 }
